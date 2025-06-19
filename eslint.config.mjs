346abb8b4a7f15e6a11ddb377ca28e23a1b8caa8
kaI1ka <kaka@bifengzhici.cn>
2025-06-20 00:17:35 +0800

    chore: 配置oxlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import { icebreaker } from '@icebreakers/eslint-config'
+import oxlint from 'eslint-plugin-oxlint'
 
 export default icebreaker({
   vue: true,
   tailwindcss: true,
   weapp: true,
-})
+}, oxlint.configs['flat/all'])
