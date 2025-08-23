21aceee7126b744e833b271e8298206ee62601ae
kaI1ka <kaka@bifengzhici.cn>
2025-08-24 00:04:04 +0800

    chore: tailwindcss的eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
 import antfu from '@antfu/eslint-config'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 
 export default antfu({
   vue: {
     overrides: {
       'no-console': 'off',
     },
+
   },
   typescript: {
     overrides: {
       'no-console': 'off',
     },
   },
-})
+}, tailwindcss.configs['flat/recommended'])
