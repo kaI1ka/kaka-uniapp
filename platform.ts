cbf6fc1f856de96731dce3801cb4ecc128dc1481
kaI1ka <kaka@bifengzhici.cn>
2026-01-20 21:40:04 +0800

    fix: 修复鸿蒙下并未关闭weapp-tailwindcss的问题

--- platform.ts
+++ platform.ts
@@ -1,8 +1,9 @@
 import process from 'node:process'
 
 const isH5 = process.env.UNI_PLATFORM === 'h5'
-const isApp = process.env.UNI_PLATFORM === 'app'
-const WeappTailwindcssDisabled = isH5 || isApp
-const isMp = !isH5 && !isApp
+const isAppPlus = process.env.UNI_PLATFORM === 'app'
+const isAppHarmony = process.env.UNI_PLATFORM === 'app-harmony'
+const WeappTailwindcssDisabled = isH5 || isAppPlus || isAppHarmony
+const isMp = !isH5 && !isAppPlus && !isAppHarmony
 
-export { isApp, isH5, isMp, WeappTailwindcssDisabled }
+export { isAppHarmony, isAppPlus, isH5, isMp, WeappTailwindcssDisabled }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 /** 应用名称 */
 export const appName = 'kaka-uniapp'
 /** 主要主题颜色 */
-export const primaryColor = '##4d80f0'
+export const primaryColor = '#4d80f0'
